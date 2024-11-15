c745d0238408246e14e60ec35acb16d7d8853544
Melrizzuto <melrizzuto4@gmail.com>
2024-11-15 14:52:34 +0100

    completed snack 4 js

--- snack-4/script.js
+++ snack-4/script.js
@@ -7,3 +7,10 @@ const posts = [
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+
+const authorNames = posts.map(function (element) {
+  return element = element.author;
+});
+
+console.log(authorNames);
+
